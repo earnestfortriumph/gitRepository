714de5e0892f49c073d91c61633223f80042ccfa
Zyx <874506358@qq.com>
2018-04-14 23:39:48 +0800

    using yii to creat a blog

---
+++ blog/protected/views/site/pages/about.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - About';
+$this->breadcrumbs=array(
+	'About',
+);
+?>
+<h1>About</h1>
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<footer>@暨南人网络联盟提供技术支持</footer>
